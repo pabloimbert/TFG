b99c9166ced2e159f07da2a7a2b52861691a52d1
Pablo <pimbert@ucm.es>
2022-03-17 18:15:46 -0400

    Stream working Database insert tweets working

--- code/twitter/stream.py
+++ code/twitter/stream.py
@@ -3,6 +3,7 @@ import tweepy
 import emoji
 import os
 from dotenv import load_dotenv , find_dotenv
+from pymongo import MongoClient
 import pandas as pd
 import ssl
 
@@ -23,11 +24,15 @@ MOST_RECENT_ID = 0
 # set up a new class using tweepy.StreamListener
 
 class SimpleListener(tweepy.Stream):
+    client = MongoClient()
+    db = client['tweet_stream']
+    collection = db['test']
+
     def on_status(self, status):
         # code to run each time the stream receives a status
-
-        # AQUI METERIAMOS EL STATUS EN MONGO
-        print(status.text)
+        post = {"text": status.text}
+        self.collection.insert_one(post)
+        print("on_status")
 
     def on_direct_message(self, status):
         # code to run each time the stream receives a direct message
@@ -35,11 +40,6 @@ class SimpleListener(tweepy.Stream):
         # AQUI METERIAMOS EL STATUS EN MONGO
         print(status.text)
 
-    def on_data(self, status):
-        # code to run each time you receive some data (direct message, delete, profile update, status,...)
-
-        # AQUI METERIAMOS EL STATUS EN MONGO
-        print(status.text)
 
     def on_error(self, staus_code):
         # code to run each time an error is received
@@ -49,9 +49,11 @@ class SimpleListener(tweepy.Stream):
             return True
 
 def main():
-    freq_dict = pd.read_csv("../dict/FREQUENCIES_DIC.csv")
+
+
+    freq_dict = pd.read_csv("../../dict/FREQUENCIES_DIC.csv")
     load_dotenv(find_dotenv("env/TwitterTokens.env"))
-    tweepy_stream = SimpleListener(os.getenv('API_KEY'), os.getenv('API_KEY_SECRET'), os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
+    tweepy_stream = SimpleListener(os.getenv('API_KEY'), os.getenv('API_KEY_SECRET'), os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET') )
     tweepy_stream.filter(languages=['es'], track=[freq_dict["WORD"][0],freq_dict["WORD"][1],freq_dict["WORD"][2],freq_dict["WORD"][3],freq_dict["WORD"][4],freq_dict["WORD"][5],
                                                   freq_dict["WORD"][6],freq_dict["WORD"][7],freq_dict["WORD"][8],freq_dict["WORD"][9],freq_dict["WORD"][10],freq_dict["WORD"][11],
                                                   freq_dict["WORD"][12],freq_dict["WORD"][13],freq_dict["WORD"][14],freq_dict["WORD"][15],freq_dict["WORD"][16],freq_dict["WORD"][17],
