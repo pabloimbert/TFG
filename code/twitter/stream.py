50cc7725ca9c44a358cc1ef4bba44ab209c35204
Pablo <pimbert@ucm.es>
2022-03-18 17:28:17 -0400

    Stream improve

--- code/twitter/stream.py
+++ code/twitter/stream.py
@@ -28,18 +28,44 @@ class SimpleListener(tweepy.Stream):
     db = client['tweet_stream']
     collection = db['test']
 
+
+
+
     def on_status(self, status):
         # code to run each time the stream receives a status
-        post = {"text": status.text}
+        l_hashtags=[]
+        tweet_id = status.id_str
+
+        if hasattr(status, "retweeted_status"):  # Check if Retweet
+            try:
+                text = status.retweeted_status.extended_tweet['full_text']
+                l_hashtags = status.retweeted_status.extended_tweet['entities']['hashtags']
+            except AttributeError:
+                text = status.retweeted_status.text
+                l_hashtags = status.retweeted_status.entities['hashtags']
+        else:
+            try:
+                text = status.extended_tweet["full_text"]
+                l_hashtags = status.extended_tweet['entities']['hashtags']
+            except AttributeError:
+                text = status.text
+                l_hashtags = status.entities['hashtags']
+
+
+        user = status.user.screen_name
+        link = "https://twitter.com/" + user + "/status/" + tweet_id
+
+        date = status.created_at
+        n_likes = status.favorite_count
+        n_retweets = status.retweet_count
+        n_replies = status.reply_count
+
+
+        post = {'link': link, 'id': tweet_id, 'text': text, 'user': user, 'date': date, 'likes': n_likes,
+         'retweets': n_retweets, 'replies': n_replies, 'hashtags': l_hashtags}
+
         self.collection.insert_one(post)
         print("on_status")
-
-    def on_direct_message(self, status):
-        # code to run each time the stream receives a direct message
-
-        # AQUI METERIAMOS EL STATUS EN MONGO
-        print(status.text)
-
 
     def on_error(self, staus_code):
         # code to run each time an error is received
@@ -50,14 +76,14 @@ class SimpleListener(tweepy.Stream):
 
 def main():
 
-
     freq_dict = pd.read_csv("../../dict/FREQUENCIES_DIC.csv")
     load_dotenv(find_dotenv("env/TwitterTokens.env"))
-    tweepy_stream = SimpleListener(os.getenv('API_KEY'), os.getenv('API_KEY_SECRET'), os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET') )
+    tweepy_stream = SimpleListener(os.getenv('API_KEY'), os.getenv('API_KEY_SECRET'), os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'),daemon = True)
     tweepy_stream.filter(languages=['es'], track=[freq_dict["WORD"][0],freq_dict["WORD"][1],freq_dict["WORD"][2],freq_dict["WORD"][3],freq_dict["WORD"][4],freq_dict["WORD"][5],
                                                   freq_dict["WORD"][6],freq_dict["WORD"][7],freq_dict["WORD"][8],freq_dict["WORD"][9],freq_dict["WORD"][10],freq_dict["WORD"][11],
                                                   freq_dict["WORD"][12],freq_dict["WORD"][13],freq_dict["WORD"][14],freq_dict["WORD"][15],freq_dict["WORD"][16],freq_dict["WORD"][17],
                                                   freq_dict["WORD"][18],freq_dict["WORD"][19]])
+
 
 if __name__ == "__main__":
     # calling main function
